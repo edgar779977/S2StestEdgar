49abf29170ce7bdc563617b1c050fe99b2bba821
edgar779977 <alaverdyan.edo70@gmail.com>
2024-08-13 16:48:58 +0400

    succes

--- src/components/Services/Services.vue
+++ src/components/Services/Services.vue
@@ -2,12 +2,12 @@
   <section class="mt-5 mb-5 pb-5 pt-5">
     <div class="container">
       <div class="row">
-        <div v-for="item in items" class="col-12 col-md-6 col-lg-3">
+        <div v-for="item in items" :key="item.title" class="col-12 col-md-6 col-lg-3">
           <div class="box">
             <img class="img-fluid glob-img" :src="item.img" alt="glob">
             <h5 class="m-0">{{ item.title }}</h5>
             <hr class="divider">
-            <p v-for="point in item.points" class="m-0">{{ point }}</p>
+            <p v-for="point in item.points" :key="point">{{ point }}</p>
           </div>
         </div>
       </div>
@@ -16,38 +16,43 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
-  name: "Services",
-  data: () => {
+  name: 'Services',
+
+  setup() {
+    const items = ref([
+      {
+        img: 'src/assets/Images/6dda272c652d788ffeef44d232ef1a11.png',
+        title: 'ԱՎՏՈՄԱՏԱՑՈՒՄ',
+        points: ['Call Center', 'CRM', 'ERP'],
+      },
+      {
+        img: 'src/assets/Images/7697f8b11d3de1f657d05e56e88d43e2.png',
+        title: 'ԹՎԱՅԻՆ ՄԱՐՔԵԹԻՆԳ',
+        points: ['Call Center', 'CRM', 'ERP'],
+      },
+      {
+        img: 'src/assets/Images/a5d1f46a670f77d3092d7666b7cd8a38.png',
+        title: 'ԱՎԱՆԴԱԿԱՆ ՄԱՐՔԵԹԻՆԳ',
+        points: ['Call Center', 'CRM', 'ERP'],
+      },
+      {
+        img: 'src/assets/Images/d3de72a5051781ffc27a658d19c74868.png',
+        title: 'ԾՐԱԳՐԱՎՈՐՈՒՄ',
+        points: ['Call Center', 'CRM', 'ERP'],
+      },
+    ]);
+
     return {
-      items: [
-        {
-          img: 'src/assets/Images/6dda272c652d788ffeef44d232ef1a11.png',
-          title: 'ԱՎՏՈՄԱՏԱՑՈՒՄ',
-          points: ['Call Center', 'CRM', 'ERP']
-        },
-        {
-          img: 'src/assets/Images/7697f8b11d3de1f657d05e56e88d43e2.png',
-          title: 'ԹՎԱՅԻՆ ՄԱՐՔԵԹԻՆԳ',
-          points: ['Call Center', 'CRM', 'ERP']
-        },
-        {
-          img: 'src/assets/Images/a5d1f46a670f77d3092d7666b7cd8a38.png',
-          title: 'ԱՎԱՆԴԱԿԱՆ ՄԱՐՔԵԹԻՆԳ',
-          points: ['Call Center', 'CRM', 'ERP']
-        },
-        {
-          img: 'src/assets/Images/d3de72a5051781ffc27a658d19c74868.png',
-          title: 'ԾՐԱԳՐԱՎՈՐՈՒՄ',
-          points: ['Call Center', 'CRM', 'ERP']
-        }
-      ]
-    }
-  }
-}
+      items,
+    };
+
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 @import "./Services.scss";
-
 </style>
--- src/components/SocialNetworks/SocialNetworks.vue
+++ src/components/SocialNetworks/SocialNetworks.vue
@@ -2,9 +2,9 @@
   <section class="my-5">
     <div class="container">
       <div class="row">
-        <div v-for="item in items" class="col-6 col-md-4 col-lg-2">
+        <div v-for="item in items" :key="item.img" class="col-6 col-md-4 col-lg-2">
           <div class="box">
-            <img class="img-fluid" :src="item.img">
+            <img class="img-fluid" :src="item.img" alt="Social Network">
           </div>
         </div>
       </div>
@@ -13,38 +13,41 @@
 </template>
 
 <script>
+import { ref } from 'vue';
 
 export default {
   name: "SocialNetworks",
-  data: () => {
+
+  setup() {
+    const items = ref([
+      {
+        img: 'src/assets/Images/Social/fb.png'
+      },
+      {
+        img: 'src/assets/Images/Social/linkedin.png'
+      },
+      {
+        img: 'src/assets/Images/Social/harvard.png'
+      },
+      {
+        img: 'src/assets/Images/Social/apple.png'
+      },
+      {
+        img: 'src/assets/Images/Social/google.png'
+      },
+      {
+        img: 'src/assets/Images/Social/hubspot.png'
+      }
+    ]);
+
     return {
-      items: [
-        {
-          img: 'src/assets/Images/Social/fb.png'
-        },
-        {
-          img: 'src/assets/Images/Social/linkedin.png'
-        },
-        {
-          img: 'src/assets/Images/Social/harvard.png'
-        },
-        {
-          img: 'src/assets/Images/Social/apple.png'
-        },
-        {
-          img: 'src/assets/Images/Social/google.png'
-        },
-        {
-          img: 'src/assets/Images/Social/hubspot.png'
-        }
-      ]
-    }
+      items
+    };
+
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
 @import "./SocialNetworks.scss";
-
 </style>
